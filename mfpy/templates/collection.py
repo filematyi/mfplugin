f5f7431f6c4e83ea8b05cd00cc1154a83c780ce5
Matyas File <matyas.file@zeiss.com>
2026-09-17 11:50:52 +0200

    new template

--- mfpy/templates/collection.py
+++ mfpy/templates/collection.py
@@ -1,9 +1,9 @@
 """Registered input templates."""
 
-from mfpy.templates.example import template as example_template
 from mfpy.templates.planner import template as planner_template
+from mfpy.templates.plan_executor import template as plan_executor
 
 templates = {
-    "Feature implementation": example_template,
     "Planner": planner_template,
+    "Plan Executor": plan_executor,
 }
---
+++ mfpy/templates/plan_executor.py
@@ -0,0 +1,5 @@
+"""Example feature implementation input template."""
+
+template = """
+Follow the instructions from the file *_plan.md.
+"""
